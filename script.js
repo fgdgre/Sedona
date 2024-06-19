29a34d443ebd18a3baea07f756593d10f6f3ca21
Dmytro <ddemin20052016@gmail.com>
2024-06-19 13:36:36 +0300

    обновил логику

--- script.js
+++ script.js
@@ -102,11 +102,121 @@ function sortHotels() {
     (page.hotels ? page.hotels : page.favoriteHotels).innerHTML = "";
     renderHotels(hotels.sort((a, b) => b.price - a.price));
   }
+
   if (selectValue == "Найпопулярніші") {
     (page.hotels ? page.hotels : page.favoriteHotels).innerHTML = "";
     renderHotels(hotels.sort((a, b) => b.rating - a.rating));
   }
 }
+
+function getFiltersForGotels(event) {
+  event.preventDefault();
+
+  // console.log(document.querySelector(".infrastructure-form").children[0].children);
+
+  const checkboxes = document.querySelectorAll(".infrastructure-checkbox-visually-hidden");
+  const radios = document.querySelectorAll(".infrastructure-radio-visually-hidden");
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      console.log(checkboxes[i].getAttribute("id"));
+    }
+  }
+
+  for (let j = 0; j < radios.length; j++) {
+    if (radios[j].checked) {
+      console.log(radios[j].getAttribute("id"));
+    }
+  }
+}
+
+function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, "#C6C6C6", "#ffffff", controlSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromSlider.value = from;
+  }
+}
+
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, "#C6C6C6", "#ffffff", controlSlider);
+  setToggleAccessible(toInput);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+  }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "#C6C6C6", "#ffffff", toSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromInput.value = from;
+  }
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, "#C6C6C6", "#ffffff", toSlider);
+  setToggleAccessible(toSlider);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+    toSlider.value = from;
+  }
+}
+
+function getParsed(currentFrom, currentTo) {
+  const from = parseInt(currentFrom.value, 10);
+  const to = parseInt(currentTo.value, 10);
+  return [from, to];
+}
+
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+  const rangeDistance = to.max - to.min;
+  const fromPosition = from.value - to.min;
+  const toPosition = to.value - to.min;
+  controlSlider.style.background = `linear-gradient(
+      to right,
+      ${sliderColor} 0%,
+      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+      ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
+      ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
+      ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget) {
+  const toSlider = document.querySelector("#toSlider");
+  if (Number(currentTarget.value) <= 0) {
+    toSlider.style.zIndex = 2;
+  } else {
+    toSlider.style.zIndex = 0;
+  }
+}
+
+const fromSlider = document.querySelector("#fromSlider");
+const toSlider = document.querySelector("#toSlider");
+const fromInput = document.querySelector("#fromInput");
+const toInput = document.querySelector("#toInput");
+fillSlider(fromSlider, toSlider, "#C6C6C6", "#ffffff", toSlider);
+setToggleAccessible(toSlider);
+
+fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
 
 function renderHotels(localHotels) {
   let i = 0;
