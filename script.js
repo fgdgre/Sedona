7a8fd8bde9311eaaccc08d4f4e03b3e983a84e0d
Dmytro <ddemin20052016@gmail.com>
2024-07-11 13:17:43 +0300

    Фикс

--- script.js
+++ script.js
@@ -183,40 +183,40 @@ function getParsed(currentFrom, currentTo) {
   return [from, to];
 }
 
-function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-  const rangeDistance = to.max - to.min;
-  const fromPosition = from.value - to.min;
-  const toPosition = to.value - to.min;
-  controlSlider.style.background = `linear-gradient(
-      to right,
-      ${sliderColor} 0%,
-      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
-      ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
-      ${sliderColor} 100%)`;
-}
+// function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+//   const rangeDistance = to.max - to.min;
+//   const fromPosition = from.value - to.min;
+//   const toPosition = to.value - to.min;
+//   controlSlider.style.background = `linear-gradient(
+//       to right,
+//       ${sliderColor} 0%,
+//       ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+//       ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+//       ${rangeColor} ${(toPosition / rangeDistance) * 100}%,
+//       ${sliderColor} ${(toPosition / rangeDistance) * 100}%,
+//       ${sliderColor} 100%)`;
+// }
 
-function setToggleAccessible(currentTarget) {
-  const toSlider = document.querySelector("#toSlider");
-  if (Number(currentTarget.value) <= 0) {
-    toSlider.style.zIndex = 2;
-  } else {
-    toSlider.style.zIndex = 0;
-  }
-}
+// function setToggleAccessible(currentTarget) {
+//   const toSlider = document.querySelector("#toSlider");
+//   if (Number(currentTarget.value) <= 0) {
+//     toSlider.style.zIndex = 2;
+//   } else {
+//     toSlider.style.zIndex = 0;
+//   }
+// }
 
-const fromSlider = document.querySelector("#fromSlider");
-const toSlider = document.querySelector("#toSlider");
-const fromInput = document.querySelector("#fromInput");
-const toInput = document.querySelector("#toInput");
-fillSlider(fromSlider, toSlider, "#C6C6C6", "#ffffff", toSlider);
-setToggleAccessible(toSlider);
+// const fromSlider = document.querySelector("#fromSlider");
+// const toSlider = document.querySelector("#toSlider");
+// const fromInput = document.querySelector("#fromInput");
+// const toInput = document.querySelector("#toInput");
+// fillSlider(fromSlider, toSlider, "#C6C6C6", "#ffffff", toSlider);
+// setToggleAccessible(toSlider);
 
-fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
-toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
-fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
-toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+// fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+// toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+// fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+// toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
 
 function renderHotels(localHotels) {
   let i = 0;
